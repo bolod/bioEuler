aa131391499dcfa26e8b87942b6d8f2b7efcf7de
Matte <matteo.meli@gmail.com>
2010-05-26 14:38:57 +0200

    added rotate function

--- eigenvector.py
+++ eigenvector.py
@@ -1,4 +1,4 @@
-from numpy import array
+#from numpy import array
 from numpy import linalg as LA
 import numpy as np
 
@@ -13,13 +13,24 @@ import numpy as np
 #   0 0 c
 # it's represented like a vector of 6 values:
 # [a, b, c, d, e, f]
-def eigenvectors(eulerTensor):
-    a, b, c, d, e, f = eulerTensor
-    matrix = array([[a, d, f],[0, b, e],[0, 0, c]])
-    w, v = LA.eig(matrix)
+def eigenvectors(tensorMatrix):
+    #a, b, c, d, e, f = eulerTensor
+    #matrix = array([[a, d, f],[0, b, e],[0, 0, c]])
+    w, v = LA.eig(tensorMatrix)
     return v               
 
+# Function to transform an array of vectors
+# by the tensor R, it receives in input an
+# array of vectors and produces a new array
+# composed of transformed vectors.
+def rotate(R, vectors):
+    result = [np.dot(R, vector) for vector in vectors]
+    return result
 
+# Not tested, use "rotate"
+def rotate2(R, vectors):
+    result = np.dot(R, vectors)
+    return result
                    
                    
     
